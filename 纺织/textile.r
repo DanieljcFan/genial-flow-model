f6f968b96f328b893ee06f7a4defc4113d1ad2cd
DanieljcFan <13051108337@163.com>
2017-08-24 16:58:18 +0800

    debug

--- 纺织/textile.r
+++ 纺织/textile.r
@@ -5,15 +5,17 @@ library(corrplot) #covariance visualization
 library(glmnet) #lasso regression
 library(car) #vif detection
 library(reshape2) #mdataframe reshape
-
 #load company report####
-load("I:/work/genial-flow/report.rda")
-#company netin
+#dataframe重命名：行业、财务指标
+#修改从报表中提取的指标
+load("E:/FJC/report.rda")
 index <- grep('纺织', report$申万二级)
 textile <- report[index,]
-na1 <- apply(textile, 1, function(x){sum(is.na(x))>0.5})
-textile <- textile[!na1,]
 report_clean <- function(name, time, y){
+  #report_clean 函数处理公司季报，计算各季度财务指标的变化。对于季报残缺不齐的用平均法补齐
+  #默认报告期为每季度末，输出后会将所有31号改为30号
+  #函数输入 name 公司名 time 报表日（POSIXlt，包含年月日） y 需计算的财务指标（numeric）
+  #函数输出 data.frame： name time y
   library(lubridate) # date management
   if(class(time)[1] != 'POSIXlt') return('Wrong: required POSIXlt for time')
   day(time) <- 27
@@ -45,6 +47,10 @@ report_clean <- function(name, time, y){
                index4[c(1:length(index4))%%4 == 2])
   index_3 <- c(index4[c(1:length(index4))%%4 == 1])
   
+  index_1 <- index_1[!is.na(index_1)]
+  index_2 <- index_2[!is.na(index_2)]
+  index_3 <- index_3[!is.na(index_3)]
+  
   time[index_1] <- time[index_1] - months(3)
   time[index_2] <- time[index_2] - months(6)
   time[index_3] <- time[index_3] - months(9)
@@ -54,27 +60,29 @@ report_clean <- function(name, time, y){
 }
 
 netin <- report_clean(textile$corp_name, textile$report_period, textile$净利润)
+netin[netin == 0] <- NA
 netin <- netin[order(netin$time),]
-netin <- aggregate(netin$y, by=list(as.yearqtr(netin$time)), mean)
+netin <- aggregate(netin$y, by=list(as.yearqtr(netin$time)), mean, na.rm = T)
 names(netin) <- c('time', 'netin')
-netin
+# netin
 
-
-setwd('I:/work/genial-flow/纺织/')
-chain <- read.csv('纺织.csv')
+#load chain factors######
+#更改路径至该行业的文件夹
+#读入数据，需对Excel表做预处理
+#注意时间列的格式（‘/’分割 or ‘-’分割）
+setwd("E:/FJC/纺织/")
+chain <- read.csv('纺织1.csv')
 names(chain)[1] <- 'time'
 chain$time <- as.POSIXlt(chain$time, format = '%Y-%m-%d')
 
-chain <- chain[year(chain$time) >= 2010 & year(chain$time) <= 2017,]
-
-#ave函数对数据集中的指标按月或季度平均
-#默认时间标签在第一列，格式为POSIXlt，包含年月信息
 Ave <- function(dat, t=1, type='season', ac = F){
+  #ave函数对数据集中的指标按月或季度平均
+  #默认时间标签在第一列，格式为POSIXlt，包含年月信息
   library(zoo)
   df <- list()
   if(type == 'season'){time <- as.yearqtr(dat[,t])}
   if(type == 'month'){time <- format(dat[,t], format='%Y-%m')}
-  dat <- cbind(dat[,t],dat[,-t])
+  dat <- dat[,c(t,c(1:ncol(dat))[-t])]
   if(ac){
     for(i in 2: ncol(dat)){
       tmp <- aggregate(dat[,i],by=list(time),max, na.rm = T)
@@ -93,14 +101,175 @@ Ave <- function(dat, t=1, type='season', ac = F){
   names(df) <- c('time',names(dat)[-1])
   return(df)
 }
-
 chain_season <- Ave(chain[, -grep('累[积计]', names(chain))], ac=F)
 tmp <- Ave(chain[, c(1,grep('累[积计]', names(chain)))], ac=T)
 chain_season <- merge(chain_season, tmp, by='time')
 
-chain_season[chain_season==0] <- NA
-na1 <- apply(chain_season[1:30,], 2, function(x){sum(is.na(x))})
+# #各省产量加总
+# index <- grep('产量.纺织', names(chain_season))
+# chain_season$纺织产量 <- rowSums(chain_season[,index])
+# chain_season <- chain_season[,-index]
+# 
+# #补充变量
+# tmp <- read.csv('纺织成本.csv')
+# names(tmp)[1] <- 'time'
+# tmp$time <- as.POSIXlt(tmp$time, format='%Y-%m-%d')
+# tmp <- Ave(tmp)
+# chain_season <- merge(chain_season, tmp, by='time')
+# 
+# #补充变量
+# tmp <- read.csv('纺织营收.csv')
+# names(tmp)[1] <- 'time'
+# tmp$time <- as.POSIXlt(tmp$time, format='%Y/%m/%d')
+# tmp1 <- Ave(tmp[,-grep('累[积计]', names(tmp))])
+# chain_season <- merge(chain_season, tmp1, by='time')
+# tmp <- Ave(tmp[,c(1,grep('累[积计]', names(tmp)))], ac=T)
+# chain_season <- merge(chain_season, tmp)
 
-sink('tmp.txt')
-na1
-sink()
+
+#删除缺失的变量
+chain_season <- chain_season[chain_season$time < 2017.5,]
+chain_season[chain_season == 0] <- NA
+na1 <- apply(chain_season, 2, function(x){sum(is.na(x))>0.5})
+# names(chain_season)[which(na1)]
+chain_season <- chain_season[,!na1]
+
+#删除常数变量
+
+
+
+#merge X and Y ####
+X <- chain_season
+# X[X==0] <- NA
+# na1 <- apply(X, 1, function(x){sum(is.na(x))>0.5})
+# X <- X[!na1,]
+# X$season <- as.factor(sub('2','1', quarters(X$time)))
+X_lag1 <- X
+X_lag1$time <- X_lag1$time + 0.25 
+names(X_lag1)[-1] <- paste0(names(X)[-1],'_lag1')
+X_lag2 <- X
+X_lag2$time <- X_lag2$time + 0.5 
+names(X_lag2)[-1] <- paste0(names(X)[-1],'_lag2')
+X_lag3 <- X
+X_lag3$time <- X_lag3$time + 0.75 
+names(X_lag3)[-1] <- paste0(names(X)[-1],'_lag3')
+X_lag4 <- X
+X_lag4$time <- X_lag4$time + 1 
+names(X_lag4)[-1] <- paste0(names(X)[-1],'_lag4')
+
+X_all <- merge(merge(merge(merge(X, X_lag1),X_lag2),X_lag3),X_lag4)
+y_lag1 <- data.frame(time=netin$time + 0.25, y_lag1=netin$netin)
+y_lag2 <- data.frame(time=netin$time + 0.5, y_lag2=netin$netin)
+y_lag3 <- data.frame(time=netin$time + 0.75, y_lag3=netin$netin)
+y_lag4 <- data.frame(time=netin$time + 1, y_lag4=netin$netin)
+X_all <- merge(y_lag1,merge(y_lag2,merge(y_lag3,merge(y_lag4,X_all))))
+X_all$season <- as.factor(sub('0','1',quarters(X_all$time)))
+
+dat_textile <- merge(netin,X_all)
+t <- dat_textile[,1]
+dat_textile <- dat_textile[,-1]
+
+# #find perfect linear X
+# corr <- cor(dat_textile[,-ncol(dat_textile)])
+# diag(corr) <- 0
+# # colnames(corr)[which(corr == 1) %% ncol(corr)]
+# # colnames(corr)[ceiling(which(corr == 1)/ncol(corr))]
+# dat_textile <- dat_textile[,-unique(which(corr > 0.95) %% ncol(corr))]
+# #删除常数变量
+# index <- which(apply(dat_textile[],2,var) == 0)
+# dat_textile
+
+
+#model#####
+
+#model函数通过marginal-r2筛选自变量，利用vif限制自相关性
+#must为手动设定的必须加入模型的变量列号
+#输出$mode为模型，$index为入选模型的自变量列号
+Model <- function(Y,X, must=c(),vif=15, method=c('aic','adj.r2')){
+  sst <- var(Y)
+  r2 <- c()
+  for(i in 1:ncol(X)){
+    lm0 <- lm(Y~., data = as.data.frame(cbind(Y,X[,i])))
+    tmp <- 1- var(lm0$residuals)/sst
+    r2 <- c(r2, tmp)}
+  
+  index <- must
+  if(method == 'aic'){
+    aic0 <- extractAIC(lm(Y~., data = as.data.frame(cbind(Y,X[,index]))))[2]
+    for(i in order(r2, decreasing = T)){
+      
+      if(length(index) == 0){index <- i}
+      if(i %in% index) {next}
+     lm1 <- lm(Y~., data = as.data.frame(cbind(Y, X[,c(index,i)])))
+      if(max(vif(lm1)) > vif){next}
+      aic1 <- extractAIC(lm1)[2]
+      if(aic1 < aic0){
+        index <- c(index,i)
+        aic0 <- aic1}}
+  }
+  if(method == 'adj.r2'){
+    lm0 <- lm(Y~., data = as.data.frame(cbind(Y,X[,index])))
+    r2_0 <- summary(lm0)$adj.r.squared
+    for(i in order(r2, decreasing = T)){
+      if(length(index) == 0){index <- i}
+      if(i %in% index) {next}
+      lm1 <- lm(Y~., data = as.data.frame(cbind(Y, X[,c(index,i)])))
+      if(max(vif(lm1)) > vif){next}
+      r2_1 <- summary(lm1)$adj.r.squared
+      if(r2_1 > r2_0){
+        index <- c(index,i)
+        r2_0 <- r2_1}}
+  }
+  
+  
+  model <- lm(Y~., data = as.data.frame(cbind(Y, X[,index])))
+  return(list(model=model,index=index))
+}
+
+m <- Model(dat_textile$netin,
+           dat_textile[-1],
+           vif=10,
+           must = ncol(dat_textile)-1,
+           method = 'aic'
+)
+
+mean(abs(m$model$residuals/dat_textile$netin))
+summary(m$model)
+vif(m$model)
+
+df <- data.frame(time=t,
+                 actual=dat_textile$netin,
+                 pred=m$model$fitted.values)
+df <- melt(df, id='time')
+ggplot(df, aes(time, value, color=variable)) + geom_line() +
+  labs(title = '纺织行业净利润')
+
+
+# tmp <- dat_textile[,m$index+1]
+# names(tmp) <- ''
+# corr <- cor(tmp)
+# corrplot.mixed(corr, diag = 'n', tl.pos = 'lt')
+
+index <- m$index[-c(2,4)]
+lm1 <- lm(netin~., data = dat_textile[,c(0,index)+1])
+mean(abs(lm1$residuals/dat_textile$netin))
+summary(lm1)
+vif(lm1)
+
+pred <- predict(lm1,X_all[X_all$time == '2017 Q2',index+1],se.fit = T,interval = 'confidence')
+df <- data.frame(time=t,
+                 actual=dat_textile$netin,
+                 pred=lm1$fitted.values)
+# df <- melt(df, id='time')
+# ggplot(df, aes(time, value, color=variable)) + geom_line() +
+#    labs(title = '纺织行业净利润')
+# write.csv(df, file = '纺织行业净利润.csv')
+# sink('纺织行业净利润模型.txt')
+# summary(lm1)
+# lm1$coefficients
+# print('置信区间:')
+# pred$fit
+# print('标准差:')
+# pred$se.fit
+# sink()
+
